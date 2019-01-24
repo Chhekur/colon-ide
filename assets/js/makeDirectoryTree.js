173389b486873dd7dbf11ab407de8900f8a9186d
Chhekur <820121223505e@gmail.com>
2019-01-24 15:05:00 +0530

    Delete makeDirectoryTree.js

--- assets/js/makeDirectoryTree.js
+++
@@ -1,7 +0,0 @@
-
-function makeMyDirectoryTree(structure){
-    console.log(structure);
-    let response = '<ul class="file-tree"><li><a href="#"><span class = "label">' + structure.name + '</span></a></li></ul>';
-    $('#project-structure').html(response);
-    $(".file-tree").filetree();
-}
