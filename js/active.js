6b1da65a99fd1cdc6de0bbe0e23a4c3f0bb6022f
Chhekur <820121223505e@gmail.com>
2019-02-18 18:37:10 +0600

    initial commit

---
+++ js/active.js
@@ -0,0 +1,148 @@
+(function ($) {
+    'use strict';
+
+    // [ JS Active Code Index ]
+
+    // :: 1.0 Owl Carousel Active Code
+    // :: 2.0 Slick Active Code
+    // :: 3.0 Footer Reveal Active Code
+    // :: 4.0 ScrollUp Active Code
+    // :: 5.0 CounterUp Active Code
+    // :: 6.0 onePageNav Active Code
+    // :: 7.0 Magnific-popup Video Active Code
+    // :: 8.0 Sticky Active Code
+    // :: 9.0 Preloader Active code
+
+    // :: 1.0 Owl Carousel Active Code
+    if ($.fn.owlCarousel) {
+        $(".welcome_slides").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            smartSpeed: 1500,
+            nav: true,
+            navText: ["<i class='pe-7s-angle-left'</i>", "<i class='pe-7s-angle-right'</i>"]
+        });
+        $(".app_screenshots_slides").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            smartSpeed: 800,
+            margin: 30,
+            center: true,
+            dots: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 3
+                },
+                992: {
+                    items: 5
+                }
+            }
+        });
+    }
+
+    // :: 2.0 Slick Active Code
+    if ($.fn.slick) {
+        $('.slider-for').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 500,
+            arrows: false,
+            fade: true,
+            asNavFor: '.slider-nav'
+        });
+        $('.slider-nav').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            speed: 500,
+            asNavFor: '.slider-for',
+            dots: true,
+            centerMode: true,
+            focusOnSelect: true,
+            slide: 'div',
+            autoplay: true,
+            centerMode: true,
+            centerPadding: '30px',
+            mobileFirst: true,
+            prevArrow: '<i class="fa fa-angle-left"></i>',
+            nextArrow: '<i class="fa fa-angle-right"></i>'
+        });
+    }
+
+    // :: 3.0 Footer Reveal Active Code
+    if ($.fn.footerReveal) {
+        $('footer').footerReveal({
+            shadow: true,
+            shadowOpacity: 0.3,
+            zIndex: -101
+        });
+    }
+
+    // :: 4.0 ScrollUp Active Code
+    if ($.fn.scrollUp) {
+        $.scrollUp({
+            scrollSpeed: 1500,
+            scrollText: '<i class="fa fa-angle-up"></i>'
+        });
+    }
+
+    // :: 5.0 CounterUp Active Code
+    if ($.fn.counterUp) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 2000
+        });
+    }
+
+    // :: 6.0 onePageNav Active Code
+    if ($.fn.onePageNav) {
+        $('#nav').onePageNav({
+            currentClass: 'active',
+            scrollSpeed: 2000,
+            easing: 'easeOutQuad'
+        });
+    }
+
+    // :: 7.0 Magnific-popup Video Active Code
+    if ($.fn.magnificPopup) {
+        $('.video_btn').magnificPopup({
+            disableOn: 0,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: true,
+            fixedContentPos: false
+        });
+    }
+
+    $('a[href="#"]').click(function ($) {
+        $.preventDefault()
+    });
+
+    var $window = $(window);
+
+    if ($window.width() > 767) {
+        new WOW().init();
+    }
+
+    // :: 8.0 Sticky Active Code
+    $window.on('scroll', function () {
+        if ($window.scrollTop() > 48) {
+            $('.header_area').addClass('sticky slideInDown');
+        } else {
+            $('.header_area').removeClass('sticky slideInDown');
+        }
+    });
+
+    // :: 9.0 Preloader Active code
+    $window.on('load', function () {
+        $('#preloader').fadeOut('slow', function () {
+            $(this).remove();
+        });
+    });
+
+})(jQuery);
